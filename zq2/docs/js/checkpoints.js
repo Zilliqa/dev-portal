a4904e3048d0355419b5812e614fdc3cc3378aef
Richard Watts <108257153+rrw-zilliqa@users.noreply.github.com>
2025-03-10 12:50:53 +0000

    (feat) #98 - permanently display changeover checkpoints. (#100)

--- zq2/docs/js/checkpoints.js
+++ zq2/docs/js/checkpoints.js
@@ -36,7 +36,7 @@ async function generateTableRow(apiUrl, baseUrl, value) {
   // Find the block hash
   return getBlockHash(apiUrl, value.block).then(
     (h) => {
-      return `<tr><td><a href="${baseUrl}/${value.key}">${value.block}</a></td><td>${h}</td><td>${humanSize(value.size)}</td></tr>`;
+      return `<tr><td><a href="${baseUrl}/${value.key}">${value.block}</a></td><td><code>${h}</code></td><td>${humanSize(value.size)}</td></tr>`;
     });
 }
 
@@ -50,41 +50,67 @@ async function fillCheckpointTableFromElem(elem) {
       parser = new DOMParser();
       data.text().then((docText) => {
         xmlDoc = parser.parseFromString(docText, "text/xml");
+        let switchoverBlock = null;
         sortedCheckpoints = []
         var checkpoints = xmlDoc.getElementsByTagName("Contents");
         for (let i = 0;i < checkpoints.length; ++i) {
           let c = checkpoints[i];
           let key = c.getElementsByTagName("Key")[0].textContent;
           let size = c.getElementsByTagName("Size")[0].textContent;
-          // Now we need the block number.
-          let block = null;
-          if (key.startsWith("previous/")) {
-            block = parseInt(key.substring(10))
+          let isSwitchover = false;
+          if (key.startsWith("switchover/")) {
+            block = parseInt(key.substring(11))
+            switchoverBlock = { key, block, size }
           } else {
-            block = parseInt(key)
+            if (key.startsWith("previous/")) {
+              block = parseInt(key.substring(10))
+            } else {
+              block = parseInt(key)
+            }
+            sortedCheckpoints.push({ key, block, size })
           }
-          sortedCheckpoints.push({ key, block, size })
         }
         sortedCheckpoints.sort( (a,b) => b.block - a.block );
+        let contents = ""
         const sliced = sortedCheckpoints.slice(0, nrCheckpoints);
-        if (sliced.length > 0) {
-          const promises = sliced.map( (v) => generateTableRow(apiUrl, listUrl, v) );
-          Promise.all(promises).then((values) => {
-            const tableContents = values.reduce( (acc,v) => acc + v, "")
-            const tableHeader = "<tr>" +
-                  "<th>Checkpoint</th>" +
-                  "<th>Hash</th>" +
-                  "<th>Size</th>" +
-                  "</tr>";
-            elem.innerHTML = "<table>" + tableHeader + tableContents + "</table>";
-          });
-        } else {
-          elem.innerHTML = '<p><i>There are no checkpoints currently stored for this network</i></p>';
-        } })
-    }
-  );
+        const tableHeader = "<tr>" +
+              "<th>Checkpoint</th>" +
+              "<th>Hash</th>" +
+              "<th>Size</th>" +
+              "</tr>";
+        new Promise(function(resolve, reject) {
+          if (sliced.length > 0) {
+            const promises = sliced.map( (v) => generateTableRow(apiUrl, listUrl, v) );
+            Promise.all(promises).then(function (values) {
+              const tableContents = values.reduce( (acc,v) => acc + v, "")
+              contents += "<table>" + tableHeader + tableContents + "</table>";
+              resolve(contents);
+            }, function (err) {
+              console.log("Err - " + err);
+            });
+          } else {
+            contents +=  '<p><i>There are no checkpoints currently stored for this network</i></p>';
+            resolve(contents);
+          }
+        }).then(function (contents) {
+          if (switchoverBlock) {
+            let tableData = generateTableRow(apiUrl, listUrl, switchoverBlock).then((value) => {
+              contents += "<p>The checkpoint at which this network switched from ZQ1 was:</p>";
+              contents += "<table>" + tableHeader + value + "</table>";
+              elem.innerHTML = contents;
+            });
+          } else {
+            elem.innerHTML = contents;
+          }
+        });
+      });
+    },
+    (err) => {
+      console.log(err);
+      elem.innerHTML = `Unable to fetch checkpoint list - please try again later; check browser console for details`;
+    });
 }
-
+  
 
 function fillInCheckpoints() {
   var ids = document.getElementsByClassName("zq2_checkpoints");
